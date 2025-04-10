eef8581bc0c60e8b79613fd3e12f3ffc3b892ec7
Ramadan54 <ramadan.asani@edu.tbz.ch>
2025-04-10 12:09:58 +0200

    Delete untitled directory

--- untitled/src/Config.java
+++
@@ -1,7 +0,0 @@
-public class Config {
-    public static final double NOISE_CHANCE = 0.25;
-    public static final String INPUT_PATH = "resources/original.png";
-    public static final String NOISE_OUTPUT_PATH = "resources/verrauscht.png";
-    public static final String DENOISED_OUTPUT_PATH = "resources/entrauscht.png";
-    public static final String IMAGE_FORMAT = "png";
-}
